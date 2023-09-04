a64eba093264791c4656d37bf95efd37bb43dcd8
Sipra <dash.sipra.sanghamitra@gmail.com>
2023-09-04 21:05:08 +0530

    just starting

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import './App.css'
+
+function App() {
+  return <div>
+    <ChatEngine
+      height="100vh"
+      projectID='
+      47416e62-7f19-4c3b-aa99-26300404e1f5'
+      userName='sipu-sipra'
+      userSecret='sipra'
+   />
+  </div>;
+}
+
+export default App;
